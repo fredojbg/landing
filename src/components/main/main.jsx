2ce44efb84c92de452345e4b4c5eff51edf128bf
Alfredo Bertho <alfredo.jose@b8one.com>
2019-06-23 16:59:27 -0300

    Adding estilos en la primera seccion

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -5,7 +5,25 @@ class main extends Component {
   render() {
     return(
         <>
-            <p>Esto es el Main</p>
+           <section className="first">
+            <div className="content__one">
+              <h1>Muchos han vivido sin amor. Ninguno sin agua</h1>
+              <a className="content__link" href="#">Go to projects</a>
+              <div class="arrow__down bounce">
+                <a className="icon" href="#second">
+                  <i class="fas fa-angle-double-down"></i>
+                </a>
+              </div>
+            </div>
+           </section>
+
+           <section id="second" className="second">
+           <div className="content__two">
+              <h1>Lorem Ipsum</h1>
+              <a className="content__link" href="#">Go to projects</a>
+            </div>
+           </section>
+            
         </>
     );
   }
