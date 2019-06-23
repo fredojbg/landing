357d6d2f37b9639758f386ed9ab079019dd526a2
Alfredo Bertho <alfredo.jose@b8one.com>
2019-06-23 00:59:50 -0300

    Adding Pages and Configuraciones

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+// Importando los estilos
+import './index.scss';
+import './scss/header/index.scss';
+import './scss/main/index.scss';
+import './scss/footer/index.scss';
+
+// Importando los componentes
 import Header from './components/header/header';
 import Main from './components/main/main';
 import Footer from './components/footer/footer';
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -4,9 +4,9 @@ import React, { Component } from 'react';
 class main extends Component {
   render() {
     return(
-        <div>
-            <h1>Esto es el Main</h1>
-        </div>
+        <>
+            <p>Esto es el Main</p>
+        </>
     );
   }
 }
