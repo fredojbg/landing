da1e6d6029105142c9cba7a49db638ca31c3c884
Alfredo Bertho <alfredo.jose@b8one.com>
2019-06-23 00:34:20 -0300

    Anadiendo la estructura

---
+++ src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function header() {
+    return (
+        <div>
+            <nav>
+                <h1>Esto el Header</h1>
+            </nav>
+        </div>
+    )
+}
+
+export default header;
